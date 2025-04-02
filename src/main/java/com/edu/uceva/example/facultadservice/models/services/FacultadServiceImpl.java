0a446349c66e00651493f8d6da29728462822b8e
Angela <angela.aponte01@uceva.edu.co>
2025-04-01 19:45:16 -0500

    feat: Implementacion de la interface del servicio facultad

---
+++ src/main/java/com/edu/uceva/example/facultadservice/models/services/FacultadServiceImpl.java
@@ -0,0 +1,39 @@
+package com.edu.uceva.example.facultadservice.models.services;
+
+import com.edu.uceva.example.facultadservice.models.entities.Facultad;
+import com.edu.uceva.example.facultadservice.models.repositories.IFacultadRepository;
+
+import java.util.List;
+
+public class FacultadServiceImpl implements IFacultadService  {
+    IFacultadRepository facultadRepository;
+
+    public FacultadServiceImpl(IFacultadRepository facultadRepository) {
+        this.facultadRepository = facultadRepository;
+    }
+
+    @Override
+    public Facultad save(Facultad facultad) {
+        return facultadRepository.save(facultad);
+    }
+
+    @Override
+    public void delete(Long id) {
+        facultadRepository.deleteById(id);
+    }
+
+    @Override
+    public Facultad findById(Long id) {
+        return facultadRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Facultad update(Facultad facultad) {
+        return facultadRepository.save(facultad);
+    }
+
+    @Override
+    public List<Facultad> findAll() {
+        return (List<Facultad>) facultadRepository.findAll();
+    }
+}
