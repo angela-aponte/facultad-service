671f258ce628aca65d34008a96521f0e6c6a3250
Angela <angela.aponte01@uceva.edu.co>
2025-04-01 20:16:33 -0500

    feat: Implementacion del controlador REST para el servicio facultad

--- src/main/java/com/edu/uceva/example/facultadservice/controllers/FacultadRestController.java
+++ src/main/java/com/edu/uceva/example/facultadservice/controllers/FacultadRestController.java
@@ -1,5 +1,45 @@
 package com.edu.uceva.example.facultadservice.controllers;
 
+import com.edu.uceva.example.facultadservice.models.entities.Facultad;
+import com.edu.uceva.example.facultadservice.models.services.IFacultadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/facultad-service")
 public class FacultadRestController {
+    private IFacultadService facultadService;
+
+    @Autowired
+    public FacultadRestController(IFacultadService facultadService) {
+        this.facultadService = facultadService;
+    }
+
+    @GetMapping("/facultades")
+    public List<Facultad> getFacultades() {
+        return facultadService.findAll();
+    }
+
+    @PostMapping("/facultad")
+    public Facultad save(@RequestBody Facultad facultad) {
+        return facultadService.save(facultad);
+    }
+
+    @DeleteMapping("/facultad")
+    public void delete (@RequestBody Long id) {
+        facultadService.delete(id);
+    }
+
+    @PutMapping("/facultad")
+    public Facultad update(@RequestBody Facultad facultad) {
+        return facultadService.update(facultad);
+    }
+
+    @GetMapping("/facultad/{id}")
+    public Facultad findById(@PathVariable("id") Long id) {
+        return facultadService.findById(id);
+    }
 
 }
